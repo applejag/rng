33bf32d93361957fc45567183441ee21cd46eb97
Kalle Jillheden <kalle.jillheden@iver.se>
2021-09-03 17:18:04 +0200

    Added "english" formatter for ints
    Closes #7

--- random_int.go
+++ random_int.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"strings"
 )
 
 type randomInt struct {
@@ -44,16 +45,21 @@ func (p randomInt) IsLowerLargerThanUpper() bool {
 
 func (p randomInt) PrintRandomValue(format string) error {
 	var value = p.lower + rand.Int63n(p.upper-p.lower)
-	if format == "" {
+	switch format {
+	case "":
 		fmt.Println(value)
 		return nil
-	}
-	fmtFormat, hasFormat := intFormats[format]
-	if hasFormat {
-		fmt.Printf(fmtFormat, value)
+	case "english":
+		fmt.Println(englishFormatInt64(value))
 		return nil
+	default:
+		fmtFormat, hasFormat := intFormats[format]
+		if hasFormat {
+			fmt.Printf(fmtFormat, value)
+			return nil
+		}
+		return errInvalidFormat
 	}
-	return errInvalidFormat
 }
 
 func (p randomInt) PrintFormatsHelp() {
@@ -65,7 +71,8 @@ func (p randomInt) PrintFormatsHelp() {
   --format b               // binary, ex: 110000001111111111101110
   --format 0b              // binary with prefix, ex: 0b110000001111111111101110
   --format o               // octal, ex: 60177756
-  --format 0o              // octal with prefix, ex: 0o60177756`)
+  --format 0o              // octal with prefix, ex: 0o60177756
+  --format english         // ex: twelve million six hundred and fourty-eight thousand four hundred and thirty`)
 }
 
 var intFormats = map[string]string{
@@ -77,4 +84,102 @@ var intFormats = map[string]string{
 	"0b": "%#b\n",
 	"o":  "%o\n",
 	"0o": "%O\n",
+}
+
+var intEnglishFormats = map[int64]string{
+	0:  "zero",
+	1:  "one",
+	2:  "two",
+	3:  "three",
+	4:  "four",
+	5:  "five",
+	6:  "six",
+	7:  "seven",
+	8:  "eight",
+	9:  "nine",
+	10: "ten",
+	11: "eleven",
+	12: "twelve",
+	13: "thirteen",
+	14: "fourteen",
+	15: "fifteen",
+	16: "sixteen",
+	17: "seventeen",
+	18: "eighteen",
+	19: "nineteen",
+	20: "twenty",
+	30: "thirty",
+	40: "forty",
+	50: "fifty",
+	60: "sixty",
+	70: "seventy",
+	80: "eighty",
+	90: "ninety",
+}
+
+func englishFormatInt64(value int64) string {
+	if formatted, ok := intEnglishFormats[value]; ok {
+		return formatted
+	}
+
+	var (
+		sb            strings.Builder
+		quintrillions = value / 1e18 // max size of int64 is at quintrillions
+		quadrillions  = value % 1e18 / 1e15
+		trillions     = value % 1e15 / 1e12
+		billions      = value % 1e12 / 1e9
+		millions      = value % 1e9 / 1e6
+		thousands     = value % 1e6 / 1e3
+		mod1000       = value % 1000
+	)
+
+	writeIntEnglishFormatsWithSpace(&sb, quintrillions, "quintrillion")
+	writeIntEnglishFormatsWithSpace(&sb, quadrillions, "quadrillion")
+	writeIntEnglishFormatsWithSpace(&sb, trillions, "trillion")
+	writeIntEnglishFormatsWithSpace(&sb, billions, "billion")
+	writeIntEnglishFormatsWithSpace(&sb, millions, "million")
+	writeIntEnglishFormatsWithSpace(&sb, thousands, "thousand")
+	writeIntEnglish100sFormatsWithSpace(&sb, mod1000)
+
+	return sb.String()
+}
+
+func writeIntEnglishFormatsWithSpace(sb *strings.Builder, value int64, name string) {
+	if value == 0 {
+		return
+	}
+	writeIntEnglish100sFormatsWithSpace(sb, value)
+	writeWithSpace(sb, name)
+}
+
+func writeIntEnglish100sFormatsWithSpace(sb *strings.Builder, value int64) {
+	var (
+		hundreds = value % 1000 / 100
+		mod100   = value % 100
+		tens     = mod100 / 10
+		ones     = mod100 % 10
+	)
+	writeIntEnglishFormatsWithSpace(sb, hundreds, "hundred")
+	if mod100 != 0 {
+		if mod100 <= 20 || ones == 0 {
+			if hundreds != 0 {
+				sb.WriteString(" and")
+			}
+			writeWithSpace(sb, intEnglishFormats[mod100])
+		} else {
+			if hundreds != 0 {
+				sb.WriteString(" and")
+			}
+			writeWithSpace(sb, intEnglishFormats[tens*10])
+			sb.WriteRune('-')
+			sb.WriteString(intEnglishFormats[ones])
+		}
+	}
+}
+
+func writeWithSpace(sb *strings.Builder, value string) {
+	if sb.Len() > 0 {
+		sb.WriteByte(' ')
+	}
+	sb.WriteString(value)
 }
